5d5e0f740b262b564ed4012f0c9a7f5dca00e71d
Ognyan Todorov <ognyan.todorov@emerchantpay.com>
2019-11-20 17:24:15 +0200

    Tagging the ACM certificates

--- modules/load_balancer/certificates.tf
+++ modules/load_balancer/certificates.tf
@@ -2,4 +2,6 @@ resource "aws_acm_certificate" "cert" {
   private_key       = file("./certificates/${var.private_key}")
   certificate_body  = file("./certificates/${var.certificate_body}")
   certificate_chain = file("./certificates/${var.certificate_chain}")
+
+  tags = merge(var.default_tags, var.custom_tags, { "Name" = var.elb_name })
 }
