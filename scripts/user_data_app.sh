a5abc24bbd8af2d800c41382ce9ace7c11360996
Ognyan Todorov <ognyan.todorov@emerchantpay.com>
2019-11-20 17:25:56 +0200

    Adding the mount commands to user data

--- scripts/user_data_app.sh
+++ scripts/user_data_app.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 hostnamectl set-hostname ${custom_hostname}-$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4 |cut -d"." -f4)-$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+sudo yum update || sudo apt update
+sudo yum install -y amazon-efs-utils || \
+  sudo yum install -y nfs-utils || \
+  sudo apt-get install nfs-common
+efs_id="${efs_id}"
+moun_point="/mnt/efs"
+sudo mkdir ${moun_point}
+sudo mount -t efs -o tls $efs_id:/ ${moun_point}
+sudo echo ${efs_id}:/ ${moun_point} efs defaults,_netdev 0 0 >> /etc/fstab
