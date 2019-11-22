f1964ff3de6087fe9b8e49d1f736814a80a62a94
Ognyan Todorov <ognyan.todorov@emerchantpay.com>
2019-11-22 14:14:03 +0200

    Edit default TTL to 0

--- app_cloudfront.tf
+++ app_cloudfront.tf
@@ -6,8 +6,8 @@ module "cloudfront_app_server" {
   cloudfront_bucket_name       = "${var.custom_tags["Name"]}-app-server"
   cloudfront_s3_origin_id      = "S3-${var.custom_tags["Name"]}-app-server"
   default_cache_min_ttl        = 0
-  default_cache_default_ttl    = 86400
-  default_cache_max_ttl        = 172800
+  default_cache_default_ttl    = 0
+  default_cache_max_ttl        = 0
   custom_tags                  = var.custom_tags
 }
 
